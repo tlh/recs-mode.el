1076fb4b147d59028101459ef9c9c084e2cd1f05
tlh <thunkout@gmail.com>
2010-07-31 14:27:02 -0500

    Fixed up the new defgroup and defcustom definitions.

--- re-suggest.el
+++ re-suggest.el
@@ -70,20 +70,20 @@
 ;;
 ;;  - You'll need to set the mapping of commands to character strings
 ;;    to your liking. To do so, set the value of
-;;    `re-suggest-cmd-char-alist' to an alist of command to
+;;    `recs-cmd-char-alist' to an alist of command to
 ;;    character-string mappings like so:
 ;;
-;;    (setq re-suggest-cmd-char-alist
+;;    (setq recs-cmd-char-alist
 ;;      '((newline         . "l")
 ;;        (previous-line   . "p")
 ;;        (next-line       . "n")))
 ;;
 ;;  - You'll also need to set the mapping of command sequence regexps
 ;;    to suggestion messages. To do so, set the value of
-;;    `re-suggest-regexp-cmd-seq-alist' to an alist of
+;;    `recs-regexp-cmd-seq-alist' to an alist of
 ;;    command-sequence-regexp to suggestion-message mappings like so:
 ;;
-;;    (defvar re-suggest-regexp-cmd-seq-alist
+;;    (defvar recs-regexp-cmd-seq-alist
 ;;      '(("lpe" . "You should use `open-line' to do that.")
 ;;        ("ov"  . "You should use `scroll-other-window' to do that.")))
 ;;
@@ -94,12 +94,12 @@
 ;;  - To set the maximum length of the command sequences re-suggest
 ;;    can recognize:
 ;;
-;;    (setq re-suggest-cmd-string-length foo)
+;;    (setq recs-cmd-string-length foo)
 ;;
 ;;  - You can set the minimum number of seconds between suggestions by
-;;    setting `re-suggest-suggestion-interval':
+;;    setting `recs-suggestion-interval':
 ;;
-;;    (setq re-suggest-suggestion-interval 60)
+;;    (setq recs-suggestion-interval 60)
 ;;
 ;;    If set to nil, suggestions will be made for every match.
 ;;
@@ -124,31 +124,31 @@
 
 ;; Customizable variables
 
-(defcustom re-suggest-null-cmd-char "_"
+(defcustom recs-null-cmd-char "_"
   "String used to represent commands not defined in
-  `re-suggest-cmd-char-alist'. Value must be a string of length
+  `recs-cmd-char-alist'. Value must be a string of length
   1."
   :type 'string
   :group 'recs)
 
-(defcustom re-suggest-suggestion-interval nil
+(defcustom recs-suggestion-interval nil
   "Minimum number of seconds between suggestions. If nil, no time
   checking is performed"
   :type 'boolean
   :group 'recs)
 
-(defcustom re-suggest-ding-on-suggestion t
+(defcustom recs-ding-on-suggestion t
   "Determines whether to call `ding' when a suggestion is made."
   :type 'boolean
   :group 'recs)
 
-(defcustom re-suggest-pop-to-buffer nil
+(defcustom recs-pop-to-buffer nil
   "Determines whether to pop to a suggestion buffer in another
 window rather than sending the suggestion to the echo area."
   :type 'boolean
   :group 'recs)
 
-(defcustom re-suggest-cmd-char-alist
+(defcustom recs-cmd-char-alist
   ;; These commands won't be in any order that makes sense. I assigned
   ;; characters to them mnemonically at the beginning, before running
   ;; out of good ones, then alphabetized based on those characters to
@@ -187,13 +187,13 @@ window rather than sending the suggestion to the echo area."
     )
   "An alist mapping commands to character strings. It's used to
 convert sequences of commands into strings. The character string
-defined in `re-suggest-null-cmd-char' is reserved for commands
+defined in `recs-null-cmd-char' is reserved for commands
 not present in this list, and should not be used. Modify this
 list to suit your needs."
   :type 'alist
   :group 'recs)
 
-(defvar re-suggest-regexp-cmd-seq-alist
+(defcustom recs-regexp-cmd-seq-alist
   '(("lpe"                                . "You should use `open-line' to do that.")
     ("xs"                                 . "You should use `eval-defun' to do that.")
     ("li"                                 . "You should use `newline-and-indent' to do that.")
@@ -221,82 +221,82 @@ list to suit your needs."
 
 ;; Nonconfigurable variables
 
-(defvar re-suggest-cmd-string nil
+(defvar recs-cmd-string nil
   "A string composed of characters that map to commands in
-  `re-suggest-cmd-char-alist'.")
+  `recs-cmd-char-alist'.")
 
-(defvar re-suggest-cmd-string-length 100
-  "Length of `re-suggest-cmd-string'.")
+(defvar recs-cmd-string-length 100
+  "Length of `recs-cmd-string'.")
 
-(defvar re-suggest-last-suggestion-time nil
+(defvar recs-last-suggestion-time nil
   "System seconds at which the last suggestion occured.")
 
-(defvar re-suggest-suggestion-buffer-map
+(defvar recs-suggestion-buffer-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "q") (lambda () (interactive) (throw 'exit nil)))
     map)
   "Keymap for commands in the suggestion buffer.")
 
-(defun re-suggest-current-time ()
+(defun recs-current-time ()
   "Returns the current system time in seconds."
   (let ((time (current-time)))
     (+ (* (car time) (expt 2 16))
        (cadr time)
        (/ (caddr time) 1000000.0))))
 
-(defun re-suggest-check-time ()
-  "Returns t if `re-suggest-suggestion-interval' is nil, if
-`re-suggest-last-suggestion-time' is nil or if the sum of the
+(defun recs-check-time ()
+  "Returns t if `recs-suggestion-interval' is nil, if
+`recs-last-suggestion-time' is nil or if the sum of the
 previous two has been superceded, nil otherwise."
-  (or (not re-suggest-suggestion-interval)
-      (not re-suggest-last-suggestion-time)
-      (>=  (re-suggest-current-time)
-           (+ re-suggest-last-suggestion-time
-              re-suggest-suggestion-interval))))
+  (or (not recs-suggestion-interval)
+      (not recs-last-suggestion-time)
+      (>=  (recs-current-time)
+           (+ recs-last-suggestion-time
+              recs-suggestion-interval))))
 
-(defun re-suggest-record-time ()
-  "Sets `re-suggest-last-suggestion-time' with the current time
+(defun recs-record-time ()
+  "Sets `recs-last-suggestion-time' with the current time
 in seconds."
-  (setq re-suggest-last-suggestion-time (re-suggest-current-time)))
+  (setq recs-last-suggestion-time (recs-current-time)))
 
-(defun re-suggest-reset-cmd-string ()
+(defun recs-reset-cmd-string ()
   "Makes and empty cmd-string of length
-`re-suggest-cmd-string-length'."
-  (setq re-suggest-cmd-string
-        (make-string re-suggest-cmd-string-length ? )))
+`recs-cmd-string-length'."
+  (setq recs-cmd-string
+        (make-string recs-cmd-string-length ? )))
 
-(defun re-suggest-verify-cmd-string ()
-  "Verifies that `re-suggest-cmd-string' exists, is a string, and
-is of the length `re-suggest-cmd-string-length'."
-  (unless (and re-suggest-cmd-string
-               (stringp re-suggest-cmd-string)
-               (= (length re-suggest-cmd-string)
-                  re-suggest-cmd-string-length))
-    (re-suggest-reset-cmd-string))
-  re-suggest-cmd-string)
+(defun recs-verify-cmd-string ()
+  "Verifies that `recs-cmd-string' exists, is a string, and
+is of the length `recs-cmd-string-length'."
+  (unless (and recs-cmd-string
+               (stringp recs-cmd-string)
+               (= (length recs-cmd-string)
+                  recs-cmd-string-length))
+    (recs-reset-cmd-string))
+  recs-cmd-string)
 
-(defun re-suggest-record-cmd ()
-  "Appends to `re-suggest-cmd-string' the character that
-  `this-original-command' maps to in `re-suggest-cmd-char-alist',
+(defun recs-record-cmd ()
+  "Appends to `recs-cmd-string' the character that
+  `this-original-command' maps to in `recs-cmd-char-alist',
   or \" \" if no match exists."
-  (re-suggest-verify-cmd-string)
-  (setq re-suggest-cmd-string
-        (concat (subseq re-suggest-cmd-string 1)
-                (or (cdr (assoc this-original-command re-suggest-cmd-char-alist))
-                    re-suggest-null-cmd-char))))
+  (recs-verify-cmd-string)
+  (setq recs-cmd-string
+        (concat (subseq recs-cmd-string 1)
+                (or (cdr (assoc this-original-command recs-cmd-char-alist))
+                    recs-null-cmd-char))))
 
-(defun re-suggest-detect-match ()
-  "Attempts to match `re-suggest-cmd-string' against all the
-regexps in `re-suggest-regexp-cmd-seq-alist'. Returns the
+(defun recs-detect-match ()
+  "Attempts to match `recs-cmd-string' against all the
+regexps in `recs-regexp-cmd-seq-alist'. Returns the
 corresponging message if a match is found, nil otherwise."
   (catch 'result
     (let (case-fold-search)
-      (dolist (pattern re-suggest-regexp-cmd-seq-alist nil)
-        (and (string-match (car pattern) re-suggest-cmd-string)
-             (throw 'result (cons (match-string 0 re-suggest-cmd-string)
+      (dolist (pattern recs-regexp-cmd-seq-alist nil)
+        (and (string-match (car pattern) recs-cmd-string)
+             (throw 'result (cons (match-string 0 recs-cmd-string)
                                   pattern)))))))
 
-(defun re-suggest-extract-quoted (str)
+(defun recs-extract-quoted (str)
   "Extract and intern a list of strings quoted like `this' from
 STR."
   (let ((pos 0) (acc))
@@ -305,8 +305,8 @@ STR."
       (setq pos (match-end 1)))
     (nreverse acc)))
 
-(defun re-suggest-get-bindings (msg)
-  "Calls `re-suggest-extract-quoted' to extract the quoted
+(defun recs-get-bindings (msg)
+  "Calls `recs-extract-quoted' to extract the quoted
   command names from MSG, then builds a list of bindings for each
   of these commands. Returns a list of command and binding
   strings."
@@ -318,67 +318,67 @@ STR."
                      (concat (format "`%s' is bound to: " cmd)
                              (mapconcat 'key-description keys ", ")))
                     (t (format "%s has no keybindings." cmd)))))
-          (re-suggest-extract-quoted msg)))
+          (recs-extract-quoted msg)))
 
-(defun re-suggest-trigger-str (match-str)
-  "Does a reverse lookup on `re-suggest-cmd-char-alist' from the
+(defun recs-trigger-str (match-str)
+  "Does a reverse lookup on `recs-cmd-char-alist' from the
 chars in match-str, constructing a string from the command
 names."
   (mapconcat (lambda (char)
                (catch 'result
-                 (dolist (elt re-suggest-cmd-char-alist)
+                 (dolist (elt recs-cmd-char-alist)
                    (when (string= (cdr elt) (char-to-string char))
                      (throw 'result (format "`%s'" (car elt)))))))
              match-str
              ", "))
 
-(defun re-suggest-suggestion (match)
+(defun recs-suggestion (match)
   "Constructs the suggestion from MATCH."
   (destructuring-bind (match-str pattern . msg) match
     (format "re-suggest -- Press `q' to exit.\n\nYou entered the command sequence:\n\n[%s]\n\n%s\n\n%s"
-            (re-suggest-trigger-str match-str)
+            (recs-trigger-str match-str)
             msg
-            (mapconcat 'identity (re-suggest-get-bindings msg) "\n"))))
+            (mapconcat 'identity (recs-get-bindings msg) "\n"))))
 
-(defun re-suggest-goto-suggestion-buffer (suggestion)
+(defun recs-goto-suggestion-buffer (suggestion)
   "Creates the suggestion buffer. Buffer is dismissable with
 `q'."
   (save-excursion
     (save-window-excursion
-      (pop-to-buffer "*re-suggest-suggestion*")
+      (pop-to-buffer "*recs-suggestion*")
       (toggle-read-only -1)
       (erase-buffer)
       (insert suggestion)
       (toggle-read-only 1)
-      (use-local-map re-suggest-suggestion-buffer-map)
+      (use-local-map recs-suggestion-buffer-map)
       (recursive-edit))))
 
-(defun re-suggest-suggest (match)
+(defun recs-suggest (match)
   "Displays suggestion to user in a separate buffer if
-  `re-suggest-pop-to-buffer' is non-nil, or in the echo area."
-  (let ((suggestion (re-suggest-suggestion match)))
-    (if re-suggest-pop-to-buffer
-        (re-suggest-goto-suggestion-buffer suggestion)
+  `recs-pop-to-buffer' is non-nil, or in the echo area."
+  (let ((suggestion (recs-suggestion match)))
+    (if recs-pop-to-buffer
+        (recs-goto-suggestion-buffer suggestion)
       (message suggestion))))
 
-(defun re-suggest-hook ()
+(defun recs-hook ()
   "Hook function to add to `post-command-hook'."
-  (when (re-suggest-check-time)
-    (re-suggest-record-cmd)
-    (let ((match (re-suggest-detect-match)))
+  (when (recs-check-time)
+    (recs-record-cmd)
+    (let ((match (recs-detect-match)))
       (when match
-        (re-suggest-reset-cmd-string)
-        (re-suggest-record-time)
-        (when re-suggest-ding-on-suggestion (ding))
-        (re-suggest-suggest match)))))
+        (recs-reset-cmd-string)
+        (recs-record-time)
+        (when recs-ding-on-suggestion (ding))
+        (recs-suggest match)))))
 
-(defun re-suggest-enable (enable)
+(defun recs-enable (enable)
   "Enables `re-suggest-mode' when ENABLE is t, disables
 otherwise."
-  (cond (enable (add-hook 'post-command-hook 're-suggest-hook)
-                (re-suggest-reset-cmd-string)
+  (cond (enable (add-hook 'post-command-hook 'recs-hook)
+                (recs-reset-cmd-string)
                 (setq re-suggest-mode t))
-        (t      (remove-hook 'post-command-hook 're-suggest-hook)
+        (t      (remove-hook 'post-command-hook 'recs-hook)
                 (setq re-suggest-mode nil))))
 
 ;;;###autoload
@@ -391,8 +391,8 @@ Otherwise, turn off re-suggest."
   :lighter     " Sug"
   :init-value  nil
   :global      t
-  (cond (noninteractive   (re-suggest-enable nil))
-        (re-suggest-mode  (re-suggest-enable t))
-        (t                (re-suggest-enable nil))))
+  (cond (noninteractive   (recs-enable nil))
+        (re-suggest-mode  (recs-enable t))
+        (t                (recs-enable nil))))
 
 (provide 're-suggest)
